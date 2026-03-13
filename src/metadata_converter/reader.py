1abe78018e38fa10e36a3b3547576dda3cdb9d98
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-03-13 11:27:21 +0100

    extends the input capabilities by adding an ExcelReader.

---
+++ src/metadata_converter/reader.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Protocol, Union
+
+import pandas as pd
+
+
+@dataclass
+class BaseReader(ABC):
+    input: Union[str, Path]
+
+    @abstractmethod
+    def execute(self) -> dict[str, Any]:
+        pass
+
+
+class CSVReader(BaseReader):
+    def execute(self) -> dict[str, Any]:
+        return pd.read_csv(self.input, skipinitialspace=True).to_dict("index")
+
+
+class ExcelReader(BaseReader):
+    def execute(self) -> dict[str, Any]:
+        df = pd.read_excel(
+            self.input,
+            sheet_name="Researchers",
+            header=0,
+            skiprows=[0, 1, 3, 4],
+            na_values="Please select",
+        ).dropna(how="all")
+        print(df)
+        df = df.replace(r"\n", " ", regex=True)
+        df["Full Name"] = df["First Name"] + " " + df["Surname"]
+        return df.to_dict("index")
