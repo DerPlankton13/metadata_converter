56db91f2f4866d5ce3e9960029b379655b4d946e
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-03-13 11:27:47 +0100

    adds a first draft for jslond file creation.

---
+++ src/metadata_converter/exporter.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+
+from src.metadata_converter.schema_org_models import SchemaDotOrgBase
+
+
+def export_to_jsonld(schema: SchemaDotOrgBase, output_path: Path) -> None:
+    if type(output_path) is str:
+        output_path = Path(output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    schema_dict = schema.model_dump()
+    jsonld_dict = {
+        "@context": {"@vocab": "https://schema.org"},
+        "@type": schema_dict.pop("type"),
+        "@id": schema_dict.pop("id"),
+    }
+    for key, value in schema_dict.items():
+        if value is not None and value != [] and value != {}:
+            jsonld_dict[key] = value
+
+    jsonld_str = json.dumps(jsonld_dict)
+
+    output_path = output_path / (jsonld_dict["@id"] + ".jsonld")
+    output_path.write_text(jsonld_str)
