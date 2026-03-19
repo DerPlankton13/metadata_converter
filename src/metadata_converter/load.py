16172c0ba965c0a5e2b0457b2b70c916a7c4c0ec
DerPlankton13 <janhendrik.plank@awi.de>
2026-03-19 10:23:37 +0100

    simplifies the jsonld creation by using the provided aliases and directly dropping Nans during the dump.

--- src/metadata_converter/load.py
+++ src/metadata_converter/load.py
@@ -9,17 +9,8 @@ def load_to_jsonld(schema: SchemaOrgBase, output_path: Path) -> None:
         output_path = Path(output_path)
     output_path.mkdir(parents=True, exist_ok=True)
 
-    schema_dict = schema.model_dump()
-    jsonld_dict = {
-        "@context": {"@vocab": "https://schema.org"},
-        "@type": schema_dict.pop("type"),
-        "@id": schema_dict.pop("id"),
-    }
-    for key, value in schema_dict.items():
-        if value != [] and value != {} and pd.notna(value):
-            jsonld_dict[key] = value
-
-    jsonld_str = json.dumps(jsonld_dict)
+    jsonld_dict = schema.model_dump(by_alias=True, exclude_none=True)
+    jsonld_str = json.dumps(jsonld_dict, indent=2)
 
     output_path = output_path / (jsonld_dict["@id"] + ".jsonld")
     output_path.write_text(jsonld_str)
