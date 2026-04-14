73f9471f7414489774727de7c4cac4ddbbd548e2
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-04-14 18:56:31 +0200

    adds data mingling to the preprocessing, so that no postprocessing is needed.

--- src/metadata_converter/main.py
+++ src/metadata_converter/main.py
@@ -23,8 +23,7 @@ def main():
     data_dict = extract_data(config)
 
     # Transform Step
-    results = {}
-    for sheet, data in data_dict.items():
+    for schema_type, data in data_dict.items():
         data = clean_dataframe(data, config.input.cleaning)
         data = add_id(data, schema_type)
         data = convert_to_long(data)
@@ -40,31 +39,42 @@ def main():
                 exploded_df = field_df.explode("value")
                 data = pd.concat([non_field_df, exploded_df], ignore_index=True)
 
-        if config.sheet_type_mapping:
-            schema_type = config.sheet_type_mapping[sheet]
+        data_dict[schema_type] = data
 
-        else:
-            if len(config.mapping) > 1:
-                raise ConfigError(
-                    "The config.mapping is invalid. If 'sheet_type_mapping' is not "
-                    "defined, only one mapping is allowed."
-                )
-            schema_type = list(config.mapping)[0]
+    # combine data for the specific types
+    # handle Person
+    def combine_names(group):
+        first = group.loc[group.header == "author:first-name", "value"].values[0]
+        last = group.loc[group.header == "author:last-name", "value"].values[0]
+        return pd.Series({"header": "name", "value": f"{first} {last}"})
 
+    full_names = data_dict["Person"].groupby("id").apply(combine_names).reset_index()
+    data_dict["Person"] = pd.concat(
+        [data_dict["Person"], full_names], ignore_index=True
+    )
+
+    # add the people as creators
+    p = data_dict["Person"]
+    creator_ids = p[(p.header == "author:is-dataset-author") & (p.value == 1)].id
+    creators = pd.DataFrame(
+        [
+            p.loc[(p.id == id) & (p.header == "@id")].value.values[0]
+            for id in creator_ids
+        ],
+        columns=["value"],
+    )
+    creators["header"] = "creator"
+    creators["id"] = "0"
+    data_dict["Dataset"] = pd.concat(
+        [data_dict["Dataset"], creators], ignore_index=True
+    )
+
+    # create the schema
+    results = {}
+    for schema_type, data in data_dict.items():
         results[schema_type] = extract_schemas(
             data, schema_type, config.mapping[schema_type]
         )
-
-    # some postprocessing
-    for schema_type, schemas in results.items():
-        if schema_type == "Person":
-            for schema in schemas:
-                if schema.name is None:
-                    schema.name = schema.givenName + schema.familyName
-        elif schema_type == "Dataset":
-            for schema in schemas:
-                if schema.creator is None:
-                    schema.creator = [Person(id=p.id) for p in results["Person"]]
 
     # Load Step
     for schema in [s for schemas in results.values() for s in schemas]:
