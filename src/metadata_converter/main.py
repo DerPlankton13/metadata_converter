544a4bb774bd5ecc18558bbfb39c2ff7fff31159
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-04-16 11:18:44 +0200

    adds additional preprocessing capabilities.

--- src/metadata_converter/main.py
+++ src/metadata_converter/main.py
@@ -13,8 +13,7 @@ from metadata_converter.transform import (
     convert_to_long,
     extract_schemas,
 )
-
-SPLIT_AUTHORS = False
+from transform_helpers import create_full_names, split_field
 
 
 def main():
@@ -28,43 +27,13 @@ def main():
         data = clean_dataframe(data, config.input.cleaning)
         data = add_id(data, schema_type)
         data = convert_to_long(data)
-
-        # Todo: move this into a plugin
-        # split the authors
-        # if SPLIT_AUTHORS:
-        #     fields_to_split = ["Authors"]
-        #     for header in fields_to_split:
-        #         field_df = data[data.header == header].copy()
-        #         non_field_df = data[data.header != header]
-        #
-        #         field_df.value = field_df.value.str.split(r"\s*[,;&]\s*|\s+and\s+")
-        #         exploded_df = field_df.explode("value")
-        #         data = pd.concat([non_field_df, exploded_df], ignore_index=True)
-
         data_dict[schema_type] = data
 
     # combine data for the specific types
-    # handle Person
-    def combine_columns(
-        df: pd.DataFrame, col1: str, col2: str, header_name: str
-    ) -> pd.Series:
-        first = df.loc[df.header == col1, "value"].values[0]
-        last = df.loc[df.header == col2, "value"].values[0]
-        return pd.Series({"header": header_name, "value": f"{first} {last}"})
+    # ====== handle Person ======
+    data_dict["Person"] = create_full_names(data_dict["Person"])
 
-    def add_full_names(data_dict: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
-        full_names = (
-            data_dict["Person"]
-            .groupby("id")
-            .apply(combine_columns, "author:first-name", "author:last-name", "name")
-            .reset_index()
-        )
-        data_dict["Person"] = pd.concat(
-            [data_dict["Person"], full_names], ignore_index=True
-        )
-        return data_dict
-
-    data_dict = add_full_names(data_dict)
+    # ====== handle main Dataset ======
     # add the people as creators
     p = data_dict["Person"]
     creator_ids = p[(p.header == "author:is-dataset-author") & (p.value == 1)].id
@@ -80,19 +49,10 @@ def main():
     data_dict["Dataset"] = pd.concat(
         [data_dict["Dataset"], creators], ignore_index=True
     )
+    data_dict["Dataset"] = split_field(data_dict["Dataset"], "dataset:keywords")
 
     # add agents to analysis
-    def split_fields(df: pd.DataFrame, fields_to_split: list[str]) -> pd.DataFrame:
-        for header in fields_to_split:
-            field_df = df[df.header == header].copy()
-            non_field_df = df[df.header != header]
-
-            field_df.value = field_df.value.str.split(r"\s*[,;&]\s*|\s+and\s+")
-            exploded_df = field_df.explode("value")
-            df = pd.concat([non_field_df, exploded_df], ignore_index=True)
-        return df
-
-    data_dict["Action"] = split_fields(data_dict["Action"], ["analysis:author-pid"])
+    data_dict["Action"] = split_field(data_dict["Action"], "analysis:author-pid")
     p_wide = p.pivot(index="id", columns="header", values="value")
 
     a = data_dict["Action"]
