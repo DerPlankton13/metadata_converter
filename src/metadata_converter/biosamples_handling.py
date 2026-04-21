42b095d2a091a9441199308419fa631fe3c3ff82
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-04-21 12:06:01 +0200

    implements a metadata fusion, which adds the units from the json metadata to the bioschemas metadata.

--- src/metadata_converter/biosamples_handling.py
+++ src/metadata_converter/biosamples_handling.py
@@ -6,14 +6,11 @@ from metadata_converter.schema_org_models.custom_models import SRA, BioSample
 from metadata_converter.schema_org_models.schemaorg_models import PropertyValue
 
 
-def get_metadata(sample_id: str) -> dict:
-
-    url = f"https://www.ebi.ac.uk/biosamples/samples/{sample_id}.ldjson"
-
+def fetch_metadata(url: str) -> dict:
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()  # raises an exception for 4xx/5xx status codes
-        data = response.json()
+        return response.json()
     except requests.exceptions.HTTPError as e:
         print(f"HTTP error: {e}")
     except requests.exceptions.ConnectionError:
@@ -21,7 +18,29 @@ def get_metadata(sample_id: str) -> dict:
     except requests.exceptions.Timeout:
         print("Request timed out")
 
-    return data
+
+def fuse_metadata(structured_metadata: dict, unstructured_metadata: dict) -> dict:
+    characteristics = unstructured_metadata.get("characteristics", {})
+
+    for prop in structured_metadata["mainEntity"]["additionalProperty"]:
+        name = prop["name"]
+        if name in characteristics and "unit" in characteristics[name][0]:
+            if len(characteristics[name]) > 1:
+                raise RuntimeError(
+                    "Could not fuse the data, several entries were found for {name} in the unstructured metadata."
+                )
+            prop["unitText"] = characteristics[name][0]["unit"]
+
+    return structured_metadata
+
+
+def get_metadata(sample_id: str) -> dict:
+
+    base_url = f"https://www.ebi.ac.uk/biosamples/samples/{sample_id}"
+    structured_metadata = fetch_metadata(base_url + ".ldjson")
+    unstructured_metadata = fetch_metadata(base_url + ".json")
+
+    return fuse_metadata(structured_metadata, unstructured_metadata)
 
 
 def find_property_value(data: dict, query: str) -> Any:
@@ -119,7 +138,9 @@ def extract_sample(data: dict, sample_id: str) -> dict:
 
     project_name = safe_extract(data, "project name")
     if project_name:
-        sample_dict["manufacturer"].append({"@type": "ResearchProject", "name": project_name})
+        sample_dict["manufacturer"].append(
+            {"@type": "ResearchProject", "name": project_name}
+        )
 
     keywords = [
         k
