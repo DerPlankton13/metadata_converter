7bc18857a271e07c4fbb69751a29189718f28942
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-05-08 17:52:33 +0200

    fixes circular validation from validate_assignment=True.

--- src/metadata_converter/biosamples/schemas.py
+++ src/metadata_converter/biosamples/schemas.py
@@ -11,16 +11,17 @@ class SRA(PropertyValue):
     alternateName: str = "SRA"
     propertyID: AnyUrl = "https://registry.identifiers.org/registry/insdc.sra"
 
-    def __init__(self, **data):
-        data["url"] = (
-            f"https://www.ebi.ac.uk/ena/browser/view/{data['value']}?dataType=SAMPLE"
-        )
-        super().__init__(**data)
-
     @field_validator("value")
     @classmethod
     def search_sra(cls, v: str) -> str:
         return search_pattern(v, re.compile(r"^[SED]R[APRSXZ]\d+$"), "SRA")
+
+    def model_post_init(self, __context) -> None:
+        object.__setattr__(
+            self,
+            "url",
+            f"https://www.ebi.ac.uk/ena/browser/view/{self.value}?dataType=SAMPLE",
+        )
 
 
 class BioSample(PropertyValue):
@@ -28,12 +29,15 @@ class BioSample(PropertyValue):
     alternateName: str = "BioSample"
     propertyID: AnyUrl = "https://registry.identifiers.org/registry/biosample"
 
-    def __init__(self, **data):
-        data["url"] = [
-            f"https://identifiers.org/biosample/{data['value']}",
-            f"https://www.ebi.ac.uk/biosamples/samples/{data['value']}",
-        ]
-        super().__init__(**data)
+    def model_post_init(self, __context) -> None:
+        object.__setattr__(
+            self,
+            "url",
+            [
+                f"https://identifiers.org/biosample/{self.value}",
+                f"https://www.ebi.ac.uk/biosamples/samples/{self.value}",
+            ],
+        )
 
     @field_validator("value")
     @classmethod
@@ -45,6 +49,8 @@ class Checklist(PropertyValue):
     name: str = "checklist"
     description: str = "There is a minimum amount of information required during ENA sample registration and all samples must conform to a defined checklist of expected metadata values. The most suitable checklist for sample registration depends on the type of the sample. (https://www.ebi.ac.uk/ena/browser/checklists)"
 
-    def __init__(self, **data):
+    @model_validator(mode="before")
+    @classmethod
+    def set_url(cls, data):
         data["url"] = f"https://www.ebi.ac.uk/ena/browser/view/{data['value']}"
-        super().__init__(**data)
+        return data
