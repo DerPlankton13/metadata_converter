d2fce7814b7fa93cafa16ade34686e31d744443b
DerPlankton13 <69301768+DerPlankton13@users.noreply.github.com>
2026-05-08 13:49:29 +0200

    finishes the rework of the extraction.

--- src/metadata_converter/biosamples/schemas.py
+++ src/metadata_converter/biosamples/schemas.py
@@ -30,8 +30,8 @@ class BioSample(PropertyValue):
 
     def __init__(self, **data):
         data["url"] = [
-            f"https://www.ebi.ac.uk/biosamples/samples/{data['value']}",
             f"https://identifiers.org/biosample/{data['value']}",
+            f"https://www.ebi.ac.uk/biosamples/samples/{data['value']}",
         ]
         super().__init__(**data)
 
@@ -39,3 +39,12 @@ class BioSample(PropertyValue):
     @classmethod
     def search_biosample(cls, v: str) -> str:
         return search_pattern(v, re.compile(r"^SAM[NED](\w)?\d+$"), "BioSample")
+
+
+class Checklist(PropertyValue):
+    name: str = "checklist"
+    description: str = "There is a minimum amount of information required during ENA sample registration and all samples must conform to a defined checklist of expected metadata values. The most suitable checklist for sample registration depends on the type of the sample. (https://www.ebi.ac.uk/ena/browser/checklists)"
+
+    def __init__(self, **data):
+        data["url"] = f"https://www.ebi.ac.uk/ena/browser/view/{data['value']}"
+        super().__init__(**data)
